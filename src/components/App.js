c7caee6c8f7beed15532894bb895e1677ef3ca41
rahul0083 <rahulme0083>
2023-02-11 18:29:38 +0530

    update1

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ const App = () => {
       <h1 className="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
       <button className='increment-button' onclick={() =>  setCount(count + 1) }>Increment</button>
     </div>
-  );
-};
+  )
+}
 
 
 export default App;
